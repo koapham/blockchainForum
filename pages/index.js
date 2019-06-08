443a86c649827cf569a5599dbc6d53750d4f0b57
koapham <47772920+koapham@users.noreply.github.com>
2019-06-08 14:56:22 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MobileDetect from 'mobile-detect';
-import { Card, Image, Statistic, Divider, Grid, Responsive, Rating } from 'semantic-ui-react';
+import {Statistic, Divider, Grid, Responsive, Table, Message} from 'semantic-ui-react';
 import { ethers } from 'ethers';
 import factory from '../ethereum/factory';
 import Rental from '../ethereum/rental';
@@ -9,6 +9,8 @@ import Layout from '../components/Layout';
 import { getWidthFactory } from '../utils/device';
 import { convertToImage } from '../utils/ipfs';
 import { Link, Router } from '../routes';
+import RentalShow from './rents/show';
+import moment from 'moment';
 
 class RentalIndex extends Component {
 
@@ -39,13 +41,20 @@ class RentalIndex extends Component {
                     return Rental(address).methods.getSummary().call();
                 })  
         );
-
+        
         summary.forEach(function(item){
             names.push(item[0]);
             owners.push(item[5]);
             deposit.push(item[3]);
             rentalFee.push(item[2]);
+
         });
+
+        const timeArray = await Promise.all(
+            availableRents.map((address)=>{
+                return Rental(address).methods.getTime().call();
+            })
+        );
 
         // const deposit2 = await Promise.all(
         //         availableRents
@@ -101,84 +110,94 @@ class RentalIndex extends Component {
         }
 
         return { deployedRents, availableRents, names, deposit, rentalFee, imageHashes, 
-                images, isMobileFromSSR, itemSumRatings, ratingCounts };
+                images, isMobileFromSSR, itemSumRatings, ratingCounts, timeArray };
     }
 
     renderRentsDesktop() {
         const items = this.props.availableRents.map((address, i) => {
             const deposit = ethers.utils.formatUnits(this.props.deposit[i], "ether");
-            const feeHour = (ethers.utils.formatUnits(this.props.rentalFee[i], "ether") * 60 * 60).toFixed(4);
-            const avgRate = this.props.ratingCounts[i]? (Math.round(this.props.itemSumRatings[i] / this.props.ratingCounts[i])) : 0;
-            return <Card key={i} link onClick={() => Router.pushRoute(`/rents/${address}`)} color='red'>
-                <Image centered src={this.props.images[i]}/>
-                <Card.Content>
-                    <Card.Header>{this.props.names[i]}</Card.Header>
-                    <Card.Description textAlign='center'>
-                    <Grid columns='equal' stackable>
-                        <Grid.Row>
-                            <Grid.Column textAlign='right'>
-                                <Statistic size='mini' color='red'>
-                                    <Statistic.Label>Deposit</Statistic.Label>
-                                    <Statistic.Value><span style={{fontSize: 17}}>{deposit} ETH</span></Statistic.Value>
-                                 </Statistic>
-                            </Grid.Column>
-                            <Grid.Column textAlign='left'>
-                                <Statistic size='mini' color='red'>
-                                    <Statistic.Label>Fee/Hour</Statistic.Label>
-                                    <Statistic.Value><span style={{fontSize: 17}}>{feeHour} ETH</span></Statistic.Value>
-                                </Statistic>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    Owner's rating: <Rating maxRating={5} rating={avgRate} icon='star' disabled/>
-                </Card.Content>
-            </Card>
+            //const feeHour = (ethers.utils.formatUnits(this.props.rentalFee[i], "ether") * 60 * 60).toFixed(4);
+            const rating = this.props.ratingCounts[i];
+            const time = this.props.timeArray[i];
+            console.log(time);
+            const timeEnd = moment.unix(time[0]).format('dddd, Do MMMM YYYY, h:mm:ss a');
+            return <Table.Row key={i}>
+                <Table.Cell textAlign='center' width={2} onClick={() => Router.pushRoute(`/rents/${address}`)} style={{cursor: 'pointer'}}>
+                    <div style={{border: 'solid 0.5px green', borderRadius: '4px'}}>
+                    <Statistic size='mini' color='red'>
+                        <Statistic.Value><span style={{fontSize: 15, color: '#6A737C'}}>{rating}</span></Statistic.Value>
+                        <Statistic.Label><span style={{fontSize: 15, color: '#6A737C'}}>votes</span></Statistic.Label>
+                    </Statistic>
+                    </div>
+                </Table.Cell>
+                <Table.Cell textAlign='center' width={2} onClick={() => Router.pushRoute(`/rents/${address}`)} style={{cursor: 'pointer'}}>
+                    <Statistic size='mini' color='red'>
+                        <Statistic.Value><span style={{fontSize: 15, color: '#6A737C'}}>0</span></Statistic.Value>
+                        <Statistic.Label><span style={{fontSize: 15, color: '#6A737C'}}>answers</span></Statistic.Label>
+                    </Statistic>
+                </Table.Cell>
+                <Table.Cell textAlign='center' width={2} onClick={() => Router.pushRoute(`/rents/${address}`)} style={{cursor: 'pointer'}}>
+                    <Statistic size='mini' color='red'>
+                        <Statistic.Value><span style={{fontSize: 15, color: '#6A737C'}}>{deposit}</span></Statistic.Value>
+                        <Statistic.Label><span style={{fontSize: 15, color: '#6A737C'}}>ethers</span></Statistic.Label>
+                    </Statistic>
+                </Table.Cell>
+                <Table.Cell textAlign='left' onClick={() => Router.pushRoute(`/rents/${address}`)} style={{cursor: 'pointer'}}>
+                    <Grid.Row textAlign='left'>
+                        <span style={{fontSize: 18, color: '#6A737C'}}><a>{this.props.names[i]}</a></span></Grid.Row>
+                    <Grid.Row textAlign='right'>
+                        <Message color='yellow' compact size='mini'
+                            header={'End time: '+timeEnd}
+                        />
+                    </Grid.Row>
+                </Table.Cell> 
+            </Table.Row>
         });
 
-        return  <Responsive fireOnMount as={Card.Group} getWidth={getWidthFactory(this.props.isMobileFromSSR)} 
-                            minWidth={Responsive.onlyTablet.minWidth} itemsPerRow={4} doubling stackable>
-                            {items}
-                </Responsive>;
+        return  <Table>
+            <Table.Body>
+                {items}
+            </Table.Body>
+        </Table>
     }
 
     renderRentsMobile() {
         const items = this.props.availableRents.map((address, i) => {
             const deposit = ethers.utils.formatUnits(this.props.deposit[i], "ether");
-            const feeHour = (ethers.utils.formatUnits(this.props.rentalFee[i], "ether") * 60 * 60).toFixed(4);
-            const avgRate = this.props.ratingCounts[i]? (Math.round(this.props.itemSumRatings[i] / this.props.ratingCounts[i])) : 0;
-            return <Card key={i} link onClick={() => Router.pushRoute(`/rents/${address}`)} color='red'>
-                <Image centered src={this.props.images[i]}/>
-                <Card.Content>
-                    <Card.Header>{this.props.names[i]}</Card.Header>
-                    <Card.Description textAlign='center'>
-                    <Grid columns='equal'>
-                        <Grid.Row>
-                            <Grid.Column textAlign='center'>
-                                <Statistic size='mini' color='red'>
-                                    <Statistic.Label>Deposit</Statistic.Label>
-                                    <Statistic.Value>{deposit} ETH</Statistic.Value>
-                                 </Statistic>
-                            </Grid.Column>
-                            <Grid.Column textAlign='center'>
-                                <Statistic size='mini' color='red'>
-                                    <Statistic.Label>Fee/Hour</Statistic.Label>
-                                    <Statistic.Value>{feeHour} ETH</Statistic.Value>
-                                </Statistic>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    Owner's rating: <Rating maxRating={5} rating={avgRate} icon='star' size='large' disabled/>
-                </Card.Content>
-            </Card>
+            //const feeHour = (ethers.utils.formatUnits(this.props.rentalFee[i], "ether") * 60 * 60).toFixed(4);
+            const rating = this.props.ratingCounts[i];
+            return <Table fixed>
+            <Table.Body>
+            <Table.Row>
+                <Table.Cell textAlign='center' width={2}>
+                    <Statistic size='mini' color='red'>
+                        <Statistic.Value><span style={{fontSize: 15, color: '#6A737C'}}>{rating}</span></Statistic.Value>
+                        <Statistic.Label><span style={{fontSize: 15, color: '#6A737C'}}>votes</span></Statistic.Label>
+                    </Statistic>
+                </Table.Cell>
+                <Table.Cell textAlign='center' width={2}>
+                    <Statistic size='mini' color='red'>
+                        <Statistic.Value><span style={{fontSize: 15, color: '#6A737C'}}>0</span></Statistic.Value>
+                        <Statistic.Label><span style={{fontSize: 15, color: '#6A737C'}}>answers</span></Statistic.Label>
+                    </Statistic>
+                </Table.Cell>
+                <Table.Cell textAlign='center' width={2}>
+                    <Statistic size='mini' color='red'>
+                        <Statistic.Value><span style={{fontSize: 15, color: '#6A737C'}}>{deposit}</span></Statistic.Value>
+                        <Statistic.Label><span style={{fontSize: 15, color: '#6A737C'}}>ethers</span></Statistic.Label>
+                    </Statistic>
+                </Table.Cell>
+                <Table.Cell textAlign='left'>
+                    <Grid.Row textAlign='left'>
+                        <span style={{fontSize: 18, color: '#6A737C'}}><a>{this.props.names[i]}</a></span></Grid.Row>
+                    <Grid.Row textAlign='right'>Duration: 2 hours</Grid.Row>
+                </Table.Cell> 
+            </Table.Row>
+            </Table.Body>
+            </Table>
         });
 
-        return <Responsive fireOnMount as={Card.Group} getWidth={getWidthFactory(this.props.isMobileFromSSR)} 
+        return <Responsive getWidth={getWidthFactory(this.props.isMobileFromSSR)} 
                             maxWidth={Responsive.onlyMobile.maxWidth} stackable doubling>
                             {items}
                 </Responsive>;
@@ -189,7 +208,7 @@ class RentalIndex extends Component {
 
         return(
             <Layout>
-                <h3>Available Rent Items</h3>
+                <h3>Questions</h3>
                 <Divider hidden/>
 
                 {this.renderRentsDesktop()}
